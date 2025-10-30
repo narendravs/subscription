5eb532cd45cc72bc7b95afada7044c6241734f30
narendravs <narendravs_mca@yahoo.com>
2025-10-30 16:03:03 +0530

    added package.json and initial dependencies

--- frontend/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{jsx,ts,html,tsx,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms")],
-};
